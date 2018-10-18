99ca01539a138e36bc9e3e79c21f79aafa136582
Marta_Kurman <marta.klachko@gmail.com>
2018-10-18 15:32:23 +0300

    added null test

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -5,23 +5,20 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertNull;
 
 
 public class MainTest {
     @Test
     public void testBuild() throws Exception {
 
-
         String line = "16,9/6/2015,1.11,99172.96,879.45,90062.62,240.79,7990.1,7762.87,227.23,0,conventional,2015,Albany";
         DateFormat format = new SimpleDateFormat("m/d/yyyy");
         Date date = format.parse("9/6/2015");
         Avocado expected = new Avocado(date.getTime(), 1.11, 99172.96, "conventional", "Albany");
-
         Avocado actual = Main.createAvocado(line.split(","));
 
-
         assertEquals(actual, expected);
-
         assertEquals(actual.getRegion(), "Albany");
         assertEquals(Long.valueOf(actual.getDate()), Long.valueOf(date.getTime()));
         assertEquals(actual.getAvgPrice(), 1.11);
@@ -29,4 +26,11 @@ public class MainTest {
 
     }
 
+    @Test
+    public void testNull()  {
+        String line ="43,fbfd,0.99,55595.74,629.46,45633.34,181.49,9151.45,8986.06,165.39,0,conventional,2015,Albany";
+        Avocado actual = Main.createAvocado(line.split(","));
+
+        assertNull(actual);
+    }
 }
