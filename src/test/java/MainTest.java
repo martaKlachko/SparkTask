79081a584317ac35ddcbd78cd769860bef3a77a8
Marta_Kurman <marta.klachko@gmail.com>
2018-10-18 16:18:04 +0300

    added  test comma separators instead of dots

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -8,6 +8,8 @@ import java.util.Date;
 import static junit.framework.TestCase.assertEquals;
 import static junit.framework.TestCase.assertNotNull;
 import static junit.framework.TestCase.assertNull;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertThat;
 
 
 public class MainTest {
@@ -42,5 +44,18 @@ public class MainTest {
         Avocado actual = Main.createAvocado(line.split(","));
 
         assertNull(actual);
+    }
+
+    @Test
+    public void testCommaSeparators() throws ParseException {
+        String line = "16,9/6/2015,1,11,99172,96,879,45,90062,62,240,79,7990,1,7762,87,227,23,0,conventional,2015,Albany";
+        DateFormat format = new SimpleDateFormat("m/d/yyyy");
+        Date date = format.parse("9/6/2015");
+        Avocado expected = new Avocado(date.getTime(), 1.11, 99172.96, "conventional", "Albany");
+        Avocado actual = Main.createAvocado(line.split(","));
+
+        assertNotNull(actual);
+        assertFalse(actual.equals(expected));
+      
     }
 }
