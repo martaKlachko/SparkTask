ec033af69be8877b602e0b69d4ad137ce96daa69
Marta_Kurman <marta.klachko@gmail.com>
2018-10-19 18:18:15 +0300

    wrote results to json files

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,21 +1,16 @@
-
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
-
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
 import org.apache.spark.util.LongAccumulator;
 import scala.Tuple2;
-import org.apache.spark.sql.SparkSession;
-
 
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-
-
 import java.util.Objects;
 
 
@@ -44,58 +39,63 @@ public class Main {
 
 
     public static void main(String[] args) throws ParseException, InterruptedException {
-      //  String csvFile = "src/main/resources/avocado.csv";
-       // String csvFile = "C:\\Users\\Marta_Kurman\\Desktop\\avocado.csv";
-      //  String csvFile = "gs://jar_storage/avocado.csv";
+        // String csvFile = "src/main/resources/avocado.csv";
+        // String csvFile = "C:\\Users\\Marta_Kurman\\Desktop\\avocado.csv";
+        //  String csvFile = "gs://jar_storage/avocado.csv";
         String csvFile = args[0];
-
         SparkConf sparkConf = new SparkConf();
         sparkConf.setAppName("Spark2Example");
         sparkConf.setMaster("local");
         JavaSparkContext context = new JavaSparkContext(sparkConf);
+        LongAccumulator accum = context.sc().longAccumulator();
 
         JavaRDD<String> rdd_records = context.textFile(csvFile);
-
         JavaRDD<String[]> splitted_csv_recordsRDD = rdd_records.map(line -> line.split(","));
 
-        LongAccumulator accum = context.sc().longAccumulator();
+
         JavaRDD<Avocado> avocadosRDD = splitted_csv_recordsRDD.map(line -> createAvocado(line));
         avocadosRDD.filter(Objects::isNull).foreach(x -> accum.add(1));
+        System.out.println("count of errors while parsing= " + accum.value());
+
+        SparkSession spark = SparkSession.builder().getOrCreate();
+
         JavaRDD<Avocado> notNullAvocadosRDD = avocadosRDD.filter(Objects::nonNull);
+        Dataset<Row> notNullAvocadosDF = spark.createDataFrame(notNullAvocadosRDD, Avocado.class);
+        notNullAvocadosDF.write().json("gs://jar_storage/result/notNullAvocados");
 
         JavaRDD<Avocado> avocadosFromBoiseRDD = notNullAvocadosRDD
                 .filter(x -> x.getRegion().equals("Boise"));
+        Dataset<Row> avocadosFromBoiseDF = spark.createDataFrame(avocadosFromBoiseRDD, Avocado.class);
+        avocadosFromBoiseDF.write().json("gs://jar_storage/result/avocadosFromBoise");
+
         Long avocadosFromBoiseCount = avocadosFromBoiseRDD.count();
         System.out.println("number of avocados from Boise: " + avocadosFromBoiseCount);
         Double maxAvgPriceFromBoise = avocadosFromBoiseRDD.map(x -> x.getAvgPrice())
                 .reduce((x, y) -> Math.max(x, y));
         System.out.println("max average price of avocados from Boise: " + maxAvgPriceFromBoise);
-        System.out.println("count of errors while parsing= " + accum.value());
 
 
         JavaPairRDD<String, Double> maxPricesByRegionRDD = notNullAvocadosRDD
                 .mapToPair(m -> new Tuple2<>(m.getRegion(), m.getAvgPrice()))
                 .reduceByKey((a, b) -> Math.max(a, b));
-        System.out.println("max prices per region");
-        System.out.println(maxPricesByRegionRDD.collect());
+        // ???? //maxPricesByRegionRDD.saveAsTextFile("gs://jar_storage/result/maxPricesByRegion");
 
-        SparkSession spark = SparkSession.builder().getOrCreate();
 
-        Dataset<Row> matchDF = spark.createDataFrame(notNullAvocadosRDD, Avocado.class);
+        Dataset<Row> avocadosDF = spark.createDataFrame(notNullAvocadosRDD, Avocado.class);
+        avocadosDF.createOrReplaceTempView("avocados");
 
-        matchDF.createOrReplaceTempView("avocados");
-
-        Dataset<Row> sqlDF = spark.sql("SELECT from_unixtime(date /1000,\"m/d/yyyy\" )as `date`, region , avgPrice FROM avocados where avgPrice=(" +
+        Dataset<Row> maxPriceDF = spark.sql("SELECT from_unixtime(date /1000,\"m/d/yyyy\" )as `date`, region , avgPrice FROM avocados where avgPrice=(" +
                 "select max(avgPrice) from avocados)");
 
-        sqlDF.show();
+
+        maxPriceDF.write().json("gs://jar_storage/result/maxPriceInAllRegions");
+
         context.close();
 
 
 //        while (true) {
 //                Thread.sleep(200);
 //            }
-
 
 
     }
